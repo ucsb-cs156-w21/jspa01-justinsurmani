8b5bb52101463523bf00fccc94ebd7b022f0afa4
Justin Surmani <justinsurmani@ucsb.edu>
2021-01-16 20:41:14 -0800

    Added Calculator component to subtract page

--- javascript/src/main/pages/Subtract.jsx
+++ javascript/src/main/pages/Subtract.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import { subtractRationalsFromUserInput } from "main/utils/RationalHelpers";
+import Calculator from "main/components/Calculator";
+
 const Subtract = () => {
-  return <div>Sub</div>;
+
+  const props = {
+    calculateFromUserInput: subtractRationalsFromUserInput,
+    title: "Subtract",
+    subtitle: "Fill in the numerator and denominator of two rational numbers (fractions) below, then click the Calculate button to see the result.",
+    sign: "-",
+  };
+
+  return <Calculator {...props} />;
 };
 
 export default Subtract;
