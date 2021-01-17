7193dccefab141e60ab3af9c69ff17e91d6e41ff
Justin Surmani <justinsurmani@ucsb.edu>
2021-01-16 20:52:22 -0800

    Added calulator component for Multiply page

--- javascript/src/main/pages/Multiply.jsx
+++ javascript/src/main/pages/Multiply.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import { multiplyRationalsFromUserInput } from "main/utils/RationalHelpers";
+import Calculator from "main/components/Calculator";
+
 const Multiply = () => {
-  return <div>Mult</div>;
+
+  const props = {
+    calculateFromUserInput: multiplyRationalsFromUserInput,
+    title: "Multiply",
+    subtitle: "Fill in the numerator and denominator of two rational numbers (fractions) below, then click the Calculate button to see the result.",
+    sign: "x",
+  }
+  return <Calculator {...props} />;
 };
 
 export default Multiply;
